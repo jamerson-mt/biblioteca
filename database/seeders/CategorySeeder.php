f3aeb17dad71eb8f78b3d3baada1d1895c76ce6d
jamerson-mt <jjamersonmt@gmail.com>
2024-09-05 23:05:38 -0300

    outra branch para tinker

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -6,26 +6,16 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 
-
 class CategorySeeder extends Seeder
 {
     public function run()
     {
-        $categories = [
-            'Ficção',
-            'Não-ficção',
-            'Fantasia',
-            'Ciência',
-            'Biografia',
-            'História',
-            'Tecnologia',
-            'Arte',
-            'Culinária',
-            'Viagem'
-        ];
-
-        foreach ($categories as $category) {
-            Category::create(['name' => $category]);
-        }
+        Category::create(['name' => 'Romance']);
+        Category::create(['name' => 'Fantasia']);
+        Category::create(['name' => 'Ficção Científica']);
+        Category::create(['name' => 'Ficção Brasileira']);
+        Category::create(['name' => 'Clássico']);
+        Category::create(['name' => 'Suspense']);
     }
 }
+
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            PublisherSeeder::class,
+            AuthorSeeder::class,
             CategorySeeder::class,
-            AuthorAndBookSeeder::class,
+            BookSeeder::class,
         ]);
     }
 }
-
