bf3ae59bbaf9dc15d1eb77de50bcbe2ddb87bc53
jamersonmt <jjamersonmt@gmail.com>
2024-09-18 18:00:07 -0300

    projeto finalizado

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Lista de livros</h1>
+        <a href="{{ route('books.create') }}" class="btn btn-primary mb-3">Adicionar Nova livro</a>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>title</th>
+                    <th>nome author</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+                    <tr>
+                        <td>{{ $book->title }}</td>
+                        <td>{{ $book->author->name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
